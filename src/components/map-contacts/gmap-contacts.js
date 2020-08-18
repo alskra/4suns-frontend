df66e1f8e783e7dc7efde3fa86d7375f0b64e6eb
alskra <krasnoyurchenko.a.s@gmail.com>
2020-08-18 10:26:15 +0400

    upd

--- src/components/map-contacts/gmap-contacts.js
+++ src/components/map-contacts/gmap-contacts.js
@@ -3,24 +3,28 @@ import {Controller} from 'stimulus';
 export default class extends Controller {
 	initialize() {
 		// eslint-disable-next-line no-undef
-		ymaps.ready(() => {
-			// eslint-disable-next-line no-undef
-			this.map = new ymaps.Map(this.element, {
-				center: this.center,
-				zoom: 15,
-			});
-
-			this.map.behaviors.disable('scrollZoom');
-			this.setMarkers();
+		this.map = new google.maps.Map(this.element, {
+			zoom: 15,
+			center: {
+				lat: this.center[0],
+				lng: this.center[1],
+			},
 		});
+
+		this.setMarkers();
 	}
 
 	setMarkers() {
 		this.markers.forEach(([title, lat, lng]) => {
-			// eslint-disable-next-line no-undef
-			this.map.geoObjects.add(new ymaps.Placemark([lat, lng], {
-				hintContent: title,
-			}));
+			// eslint-disable-next-line no-new,no-undef
+			new google.maps.Marker({
+				position: {
+					lat,
+					lng,
+				},
+				map: this.map,
+				title,
+			});
 		});
 	}
 
--- src/components/map-objects/map-objects.js
+++ src/components/map-objects/map-objects.js
@@ -1,91 +1,67 @@
 import {Controller} from 'stimulus';
 
-// eslint-disable-next-line no-undef
-class Popup extends google.maps.OverlayView {
-	constructor(position, content) {
-		super();
-		this.position = position;
-
-		// This zero-height div is positioned at the bottom of the bubble.
-		const bubbleAnchor = document.createElement('div');
-
-		bubbleAnchor.classList.add('map-objects__popup-anchor');
-		bubbleAnchor.appendChild(content);
-
-		// This zero-height div is positioned at the bottom of the tip.
-		this.containerDiv = document.createElement('div');
-		this.containerDiv.classList.add('map-objects__popup-container');
-		this.containerDiv.appendChild(bubbleAnchor);
-
-		// Optionally stop clicks, etc., from bubbling up to the map.
-		Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
-	}
-
-	/** Called when the popup is added to the map. */
-	onAdd() {
-		this.getPanes().floatPane.appendChild(this.containerDiv);
-	}
-
-	/** Called when the popup is removed from the map. */
-	onRemove() {
-		if (this.containerDiv.parentElement) {
-			this.containerDiv.parentElement.removeChild(this.containerDiv);
-		}
-	}
-
-	/** Called each frame when the popup needs to draw itself. */
-	draw() {
-		const divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
-
-		// Hide the popup when it is far out of view.
-		const display =
-			Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
-				? 'block'
-				: 'none';
-
-		if (display === 'block') {
-			this.containerDiv.style.left = `${divPosition.x}px`;
-			this.containerDiv.style.top = `${divPosition.y}px`;
-		}
-
-		if (this.containerDiv.style.display !== display) {
-			this.containerDiv.style.display = display;
-		}
-	}
-}
-
 export default class extends Controller {
 	initialize() {
 		// eslint-disable-next-line no-undef
-		this.map = new google.maps.Map(this.element, {
-			center: {
-				lat: this.center[0],
-				lng: this.center[1],
-			},
-			zoom: window.innerWidth >= 1025 ? 11 : 9,
-		});
+		ymaps.ready(() => {
+			// eslint-disable-next-line no-undef
+			this.map = new ymaps.Map(this.element, {
+				center: this.center,
+				zoom: window.innerWidth >= 1025 ? 11 : 9,
+			});
 
-		this.setObjects();
+			this.map.behaviors.disable('scrollZoom');
+			this.setObjects();
+		});
 	}
 
 	setObjects() {
-		this.objects.forEach(([title, image, lat, lng]) => {
-			const content = document.createElement('div');
-
-			content.classList.add('map-objects__popup');
-			content.innerHTML = `
-				<div class="map-objects__popup-image-box">
-					<img class="map-objects__popup-image" src="${image}" alt="${title}">
-				</div>
-				<div class="map-objects__popup-body">
-					<div class="map-objects__popup-title">${title}</div>
-				</div>
-			`;
-
+		// eslint-disable-next-line no-unused-vars
+		this.objects.forEach(([title, image, lat, lng], index) => {
 			// eslint-disable-next-line no-undef
-			const popup = new Popup(new google.maps.LatLng(lat, lng), content);
+			const placemark = new ymaps.Placemark([lat, lng], {
+				hintContent: title,
+			}, {
+				// eslint-disable-next-line no-undef
+				iconLayout: ymaps.templateLayoutFactory.createClass(`
+					<div class="map-objects__popup-container">
+						<div class="map-objects__popup-anchor">
+							<div class="map-objects__popup">
+								<div class="map-objects__popup-image-box">
+									<img class="map-objects__popup-image" src="${image}" alt="${title}">
+								</div>
+								<div class="map-objects__popup-body">
+									<div class="map-objects__popup-title">${title}</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				`),
+				// iconShape: {
+				// 	type: 'Rectangle',
+				// 	coordinates: [
+				// 		[-80, -120],
+				// 		[80, -8],
+				// 	],
+				// },
+			});
 
-			popup.setMap(this.map);
+			this.map.geoObjects.add(placemark);
+
+			// setTimeout(() => {
+			// 	const popupElt = document.querySelectorAll('.map-objects__popup')[index];
+			// 	console.log(index, document.querySelectorAll('.map-objects__popup')[index], placemark);
+			//
+			// 	placemark.options.set({
+			// 		iconShape: {
+			// 			type: 'Rectangle',
+			// 			coordinates: [
+			// 				[-80, -120],
+			// 				[80, -8],
+			// 			],
+			// 		},
+			// 	});
+			// }, 100);
 		});
 	}
 
